e3e33d80f3b9b6ff86df94e90667d79c8028e7fc
lishiyo <c.ziye.li@gmail.com>
2014-09-17 08:41:26 +0000

    git ignored config

--- twit-decision/routes/config.js
+++
@@ -1,8 +0,0 @@
-var twitterKeys = {
-  consumer_key: 'Jk8b4F8afEcmXT6SYMHGqL5m1',
-  consumer_secret: 'IvU4LJczqmmHHYlYY9BIwd92YNvU06bXJ7OicacxGTdR5LdNGc',
-  access_token: '1643673930-YkXa4pPXk5HA2RYKqw4POwLFQdC742FxUZwSWO2',
-  access_token_secret: 'sIOSgM3kh9A8p0xIQ1VUhC34b1ioaDN3m6RwMDcvVQXu5'
-}
-
-module.exports = twitterKeys
