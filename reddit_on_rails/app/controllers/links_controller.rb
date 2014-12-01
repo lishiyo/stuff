9b7d5972c2ad70fe4f7c3669b31cc7f72993679e
lishiyo <c.ziye.li@gmail.com>
2014-12-01 22:19:05 +0000

    Link form

---
+++ reddit_on_rails/app/controllers/links_controller.rb
@@ -0,0 +1,27 @@
+class LinksController < ApplicationController
+	
+	def show
+	end
+	
+	def new
+		@link = Link.new
+	end
+	
+	def create
+		link = Link.create!(link_params)
+		if link.save
+			flash[:success] = "Link submitted!"
+			redirect_to root_url
+		else
+			flash[:danger] = "Something went wrong."
+			redirect_to new_link_path
+		end
+	end
+	
+	private 
+	
+	def link_params
+		params.require(:link).permit(:url, :title)
+	end
+	
+end
--- reddit_on_rails/config/routes.rb
+++ reddit_on_rails/config/routes.rb
@@ -8,6 +8,7 @@ Rails.application.routes.draw do
 	root 'pages#index'
 	
 	resources :pages
+	resources :links
 	
   # Example of regular route:
   #   get 'products/:id' => 'catalog#view'
